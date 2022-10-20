e0a560aaeccd8eea3cb78af550829e3dbb8d4fd6
yongbeom76 <yongbeom76@gmail.com>
2022-10-20 13:35:53 +0900

    products join

--- yongbeom/w3schools/Products_CategoryID.sql
+++ yongbeom/w3schools/Products_CategoryID.sql
@@ -1,9 +1,9 @@
 -- +2: Products에 CategoryID가 5,6을 표시
 -- 내용 프로덕트 이름,공급자 이름 출력?
 
-SELECT ProductName, SupplierID 
-FROM Products
-WHERE CategoryID IN (5,6)
+SELECT Products.ProductName, Suppliers.SupplierName
+FROM Products INNER JOIN Suppliers
+	ON CategoryID IN (5,6)
 ;
 
--- 결과값 13
+-- 결과값 377
